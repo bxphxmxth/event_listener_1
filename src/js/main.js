448ca88e10355265b1795abcadb0289dc70ae3bf
bxphxmxth <fushs@hotmail.fr>
2021-06-01 14:39:30 +0200

    +exo3 à 6 + BONUS random Color

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,7 @@ console.log(myBody);
 
 myBody.appendChild(newButton)
 
+
 let changeText = () => {
      newButton.innerText = ("J'ai été cliqué");
 }
@@ -23,13 +24,129 @@ newButton.addEventListener("click", changeText);
 
 let button2 = document.createElement("button");
 myBody.appendChild(button2);
-
-let x = 0;
-button2.innerText = (`J'ai été cliqué ${x} fois`)
-
 let count = 0;
+button2.innerText = (`J'ai été cliqué ${count} fois`)
+
+
 
    button2.addEventListener("click", function() {
      count += 1;
      button2.innerText = (`J'ai été changé ${count} fois`)
    });
+
+//    03
+// > Créer un button a qui vous assignerez l'écouteur d'évenement click qui rajoutera un titre dans votre page web
+
+let button3 = document.createElement("button");
+
+myBody.appendChild(button3);
+
+let h3Button3 = document.createElement("h3");
+h3Button3.innerText = "Titre H3"
+
+console.log(h3Button3);
+
+let addText = () =>{
+     button3.appendChild(h3Button3)
+}
+
+button3.addEventListener("click",addText)
+
+
+// ## Exo 04
+// > Créer un buttton, assigner l'écouteur d'évenement click. Ce bouton rajoutera un button dans votre page web avec comme valeur Exo 04
+
+
+let button4 = document.createElement("button");
+myBody.appendChild(button4);
+
+
+
+let addButton = () =>{
+     newButton4 = document.createElement("button");
+     newButton4.innerText = "exo 4 "
+     myBody.appendChild(newButton4);
+}
+
+button4.addEventListener("click",addButton)
+
+
+// ## Exo 05
+// > Créez un paragraphe avec du texte
+// > Créez un button "modifier la couleur du paragraphe"
+// > Au clique ça changera la couleur du paragraphe
+
+
+let p = document.createElement("p");
+p.innerText = "wesh la zone";
+myBody.appendChild(p);
+
+let button5 = document.createElement("button");
+button5.innerText = "modifier la couleur du paragraphe";
+myBody.appendChild(button5)
+
+
+let changeColorP = () =>{
+     p.style.color = "red";
+}
+
+button5.addEventListener("click",changeColorP)
+
+// ## Exo 6
+// > Etape 1 :
+// > Créer un button qui crée un h2 avec une valeur "Je lance une function"
+// > Rajouter le titre sur la page web
+// > Etape 2 :
+// > Cette function doit également rajouter un boutton avec comme valeur "Modifier le titre Exo 06"
+
+// > Etape 3 :
+// > le bouton "Modifier le titre" lance une function qui modifie le texte en lui mettant sa couleur en rouge.
+
+let button6 = document.createElement("button");
+button6.innerText = "Créer un H2";
+myBody.appendChild(button6);
+
+let h26 = document.createElement("h2");
+
+h26.innerText = "Je lance une fonction";
+
+
+
+let button7 = document.createElement("button");
+button7.innerText = "Modifier le titre exo 06";
+
+
+let ajouterH2Button6 = () =>{
+     myBody.appendChild(h26);
+     myBody.appendChild(button7)
+}
+
+button6.addEventListener("click",ajouterH2Button6);
+
+
+let changerCoulModifierTitre = () =>{
+     button7.style.color = "red"
+}
+
+button7.addEventListener("click",changerCoulModifierTitre);
+
+
+// Random Color BONUS
+
+
+let buttonTest = document.createElement("button");
+buttonTest.innerText = "Random Color";
+
+myBody.appendChild(buttonTest);
+
+
+let tabColor = ["blue", "red", "green", "yellow", "grey", "white","purple","orange"];
+
+
+let changeColor = () =>{
+     let randomColor = tabColor[Math.floor(Math.random()*tabColor.length)];
+     buttonTest.style.color = randomColor;
+
+}
+
+buttonTest.addEventListener("click", changeColor)
